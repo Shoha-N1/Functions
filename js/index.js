4763e0e8adaccac2ef12152943685cbc53194035
Shoha-N1 <abduvohidovs85@gmail.com>
2023-03-14 12:09:02 +0500

    Commit

---
+++ js/index.js
@@ -0,0 +1,30 @@
+     
+                // O'rta arifmetika //
+
+//    let nums = [4,5,9]
+//    let sum = 0
+
+//    for (let i = 0; i < nums.length; i++) {
+//     sum += nums[i]
+//    }
+
+//    console.log(sum / nums.length)
+
+
+     
+                // Arifmetik progressiya //
+
+                // function ArifProgress(num1 = 0, num2) {
+                //     let num = 0;
+                //     for (let i = num1; i <= num; i++) {
+                //       num += i;
+                //     }
+                //     console.log(num);
+                //   }
+                    
+
+                // Perimetr //
+              
+                // function perimetr(a, b, c) {
+                //     return console.log(a + b + c);
+                //   }
